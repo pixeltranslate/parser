4b5ae73ef4247c0425cc96a58491e6eb51b6ae9d
ReaperMaga <reapermaga@gmail.com>
2024-07-24 22:26:06 +0300

    Added documentation to parseJSON

--- utils/parseJSON.ts
+++ utils/parseJSON.ts
@@ -1,6 +1,21 @@
 import { flatten } from 'flat'
 import type { ParseOutput } from '~/types/parse'
 
+/**
+ * Parses an object (nested json) into a flatten map
+ * Example: {
+ *     hello: {
+ *         world: "Hello World"
+ *     }
+ * }
+ * Would be converted to : {"hello.world": "Hello World}
+ *
+ * If the object you pass as the parameter is already a nested json, it will basically do nothing.
+ * If the object is invalid (undefined), it will throw an error
+ *
+ * @param object = the nested json
+ * @param delimiter = the output delimited
+ */
 export default function (object: any, delimiter: string = '.'): ParseOutput<Map<string, string>> {
   const stopWatch = Date.now()
   const result = flatten(object, {
